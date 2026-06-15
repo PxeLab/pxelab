0f49048ed79f11a699dcccd104ba22d0e7b76de3
root <tangwenjun_215@163.com>
2026-06-15 22:37:37 +0800

    fix: 添加 defer signal.Stop 防止信号处理程序泄漏

--- internal/app/app.go
+++ internal/app/app.go
@@ -44,6 +44,7 @@ func (a *App) Run(ctx context.Context) error {
 	// 信号处理
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigCh)
 
 	select {
 	case <-ctx.Done():
