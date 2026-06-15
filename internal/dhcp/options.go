97cb33d16417d2f23b7f1f5c394d51810b0a20f3
root <tangwenjun_215@163.com>
2026-06-15 22:46:22 +0800

    feat: 添加 PXE option 构建器

---
+++ internal/dhcp/options.go
@@ -0,0 +1,60 @@
+package dhcp
+
+import (
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/insomniacslk/dhcp/iana"
+)
+
+// BuildPXEOptions 构建 PXE 相关的 DHCP 选项
+func BuildPXEOptions(arch iana.Arch, nextServer string, bootFile string, menuData []byte) []dhcpv4.Option {
+	options := []dhcpv4.Option{
+		dhcpv4.OptTFTPServerName(nextServer),
+		dhcpv4.OptBootFileName(bootFile),
+	}
+
+	// Option 43 - Vendor Specific Information
+	// PXE sub-option 6: Discovery Control
+	discoveryCtrl := []byte{6, 1, 0x08}
+	_ = discoveryCtrl
+
+	// PXE sub-option 8: Boot Menu
+	if menuData != nil {
+		bootMenu := buildSubOption(8, menuData)
+		option43 := dhcpv4.OptGeneric(dhcpv4.OptionVendorSpecificInformation, bootMenu)
+		options = append(options, option43)
+	}
+
+	return options
+}
+
+func buildSubOption(subOpt byte, data []byte) []byte {
+	// 子选项编码：子选项代码(1)+长度(1)+数据(n)
+	length := len(data)
+	result := make([]byte, 0, 2+length)
+	result = append(result, subOpt)
+	result = append(result, byte(length))
+	result = append(result, data...)
+	return result
+}
+
+// DetectClientArch 从 DHCP 包读取架构类型
+func DetectClientArch(pkt *dhcpv4.DHCPv4) (iana.Arch, bool) {
+	if pkt == nil {
+		return 0, false
+	}
+	// 读取 Option 93
+	archs := pkt.ClientArch()
+	if len(archs) > 0 {
+		return archs[0], true
+	}
+	return 0, false
+}
+
+// IsPXEClient 检测客户端是否为 PXE 客户端（Option 60）
+func IsPXEClient(pkt *dhcpv4.DHCPv4) bool {
+	if pkt == nil {
+		return false
+	}
+	vci := pkt.ClassIdentifier()
+	return vci == "PXEClient"
+}
