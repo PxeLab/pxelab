8b20ba72af630a0fb0ffd299d2a57e968f6bb506
root <tangwenjun_215@163.com>
2026-06-19 21:56:36 +0800

    feat(config): add BootConfig and sub-types for iPXE boot settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,14 +24,15 @@ type GlobalConfig struct {
 }
 
 type InterfaceConfig struct {
-	Name       string         `mapstructure:"name"`
-	IP         string         `mapstructure:"ip"`
-	Subnets    []SubnetConfig `mapstructure:"subnets"`
-	DHCP       string         `mapstructure:"dhcp"` // full | proxy | hybrid | off
-	Bootloader string         `mapstructure:"bootloader"` // ipxe | pxelinux | grub2
-	TFTP       bool           `mapstructure:"tftp"`
-	HTTP       bool           `mapstructure:"http"`
-	DNS        bool           `mapstructure:"dns"`
+	Name        string         `mapstructure:"name"`
+	IP          string         `mapstructure:"ip"`
+	Subnets     []SubnetConfig `mapstructure:"subnets"`
+	DHCP        string         `mapstructure:"dhcp"`         // full | proxy | hybrid | off
+	Bootloader  string         `mapstructure:"bootloader"`  // ipxe | pxelinux | grub2
+	ChainToIPXE bool           `mapstructure:"chain_to_ipxe"` // pxelinux/grub2 → chain-load to iPXE
+	TFTP        bool           `mapstructure:"tftp"`
+	HTTP        bool           `mapstructure:"http"`
+	DNS         bool           `mapstructure:"dns"`
 }
 
 type SubnetConfig struct {
@@ -49,15 +50,13 @@ type AuthConfig struct {
 	Token string `mapstructure:"token"`
 }
 
-type BootConfig struct {
-	RootDir string `mapstructure:"root_dir"`
-}
-
 type NetbootConfig struct {
 	Enabled        bool              `mapstructure:"enabled"`
 	DefaultBoot    string            `mapstructure:"default_boot"`
 	FallbackOnline bool              `mapstructure:"fallback_online"`
 	MenuTitle      string            `mapstructure:"menu_title"`
+	ScriptTemplate string            `mapstructure:"script_template"`
+	Boot           BootConfig        `mapstructure:"boot"`
 	Sync           NetbootSyncConfig `mapstructure:"sync"`
 	Paths          NetbootPathConfig `mapstructure:"paths"`
 }
@@ -72,6 +71,56 @@ type NetbootPathConfig struct {
 	Catalog   string `mapstructure:"catalog"`
 	Scripts   string `mapstructure:"scripts"`
 	BootFiles string `mapstructure:"boot_files"`
+}
+
+type BootConfig struct {
+	RootDir         string                `mapstructure:"root_dir"`
+	DefaultMenu     DefaultMenuConfig     `mapstructure:"default_menu"`
+	ProfileBehavior ProfileBehaviorConfig `mapstructure:"profile_behavior"`
+	CatalogRedirect CatalogRedirectConfig `mapstructure:"catalog_redirect"`
+	CatalogDisplay  CatalogDisplayConfig  `mapstructure:"catalog_display"`
+}
+
+type DefaultMenuConfig struct {
+	Title   string      `mapstructure:"title"`
+	Timeout int         `mapstructure:"timeout"`
+	Default int         `mapstructure:"default"`
+	Entries []MenuEntry `mapstructure:"entries"`
+}
+
+type MenuEntry struct {
+	Label   string  `mapstructure:"label"`
+	Type    string  `mapstructure:"type"`
+	Kernel  *string `mapstructure:"kernel,omitempty"`
+	Initrd  *string `mapstructure:"initrd,omitempty"`
+	Cmdline *string `mapstructure:"cmdline,omitempty"`
+	URL     *string `mapstructure:"url,omitempty"`
+	WIM     *string `mapstructure:"wim,omitempty"`
+}
+
+type ProfileBehaviorConfig struct {
+	AppendLocal    bool   `mapstructure:"append_local"`
+	AppendNetboot  bool   `mapstructure:"append_netboot"`
+	AppendPosition string `mapstructure:"append_position"`
+}
+
+type CatalogRedirectConfig struct {
+	Enabled    bool   `mapstructure:"enabled"`
+	TargetURL  string `mapstructure:"target_url"`
+	DetectArch bool   `mapstructure:"detect_arch"`
+	Preamble   string `mapstructure:"preamble"`
+}
+
+type CatalogDisplayConfig struct {
+	Title  string         `mapstructure:"title"`
+	Groups []CatalogGroup `mapstructure:"groups"`
+}
+
+type CatalogGroup struct {
+	Name    string `mapstructure:"name"`
+	Title   string `mapstructure:"title"`
+	Enabled bool   `mapstructure:"enabled"`
+	Order   int    `mapstructure:"order"`
 }
 
 type StoreConfig struct {
@@ -91,6 +140,9 @@ func (c *Config) Validate() error {
 		}
 		if iface.Bootloader != "" && iface.Bootloader != "ipxe" && iface.Bootloader != "pxelinux" && iface.Bootloader != "grub2" {
 			return fmt.Errorf("interface %s: 无效的引导加载器: %s", iface.Name, iface.Bootloader)
+		}
+		if iface.ChainToIPXE && iface.Bootloader != "pxelinux" && iface.Bootloader != "grub2" {
+			return fmt.Errorf("interface %s: chain_to_ipxe 仅支持 pxelinux 或 grub2", iface.Name)
 		}
 	}
 	return nil
